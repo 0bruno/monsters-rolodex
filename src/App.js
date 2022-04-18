23c8e264c5c4430545c8ef3fc8319da6f1f20290
Bruno <brunobsribeiro@gmail.com>
2022-04-17 22:14:59 -0300

    add to class a constructor  and state object

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Bruno',
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hi {this.state.name}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
