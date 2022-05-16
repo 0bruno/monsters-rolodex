6bba2e318a4318e221b8c9f319d68f8e1ef5c7f7
Bruno <brunobsribeiro@gmail.com>
2022-05-16 14:10:40 -0300

    implement a hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBox from './components/search-box/search-box.component.jsx';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 
 export default function App() {
-  console.log('Render');
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
   console.log(searchField);
+  console.log('Render');
 
-  fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
-    response.json().then((users) => setMonsters(users))
-  );
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+      response.json().then((users) => setMonsters(users))
+    );
+  }, []);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
